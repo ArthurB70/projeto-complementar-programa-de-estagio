b898e4aa0740cc04f050115a42a4f0e59ea9a30d
Arthur Bernardo Coelho Moura <69174018+ArthurB70@users.noreply.github.com>
2021-02-23 21:26:28 -0300

    1.0 - Inicio Projeto

---
+++ projeto-complementar-programa-de-estagio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_complementar_programa_de_estagio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
